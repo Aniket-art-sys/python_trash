5e78f6bd70c8da6b3e0e7dcb268364a8756eb10f
Aniket-art-sys <65751787+Aniket-art-sys@users.noreply.github.com>
2024-07-21 09:42:11 +0530

    ai bug fixes tictac.py

--- tictac.py
+++ tictac.py
@@ -16,21 +16,24 @@ def print_board(board,type):
             print(convert(board,_),"|",convert(board,_+1),"|",convert(board,_+2))
         else:
             print(_,"|", _+1,"|",_+2)
-def plays(board,no):
-    while(True):
-        try:
-            board_input=int(input("enter pos according to the map"))
-        except ValueError:
-            print("Invalid input try again 0-8")
-            continue
+def plays(board,no,ai):
+    if ai and no%2==1 :
+        board[ai_move(board,no)]=-1
+    else:
+        while (True):
+            try:
+                board_input = int(input("enter pos according to the map"))
+            except ValueError:
+                print("Invalid input try again 0-8")
+                continue
 
-        if (board_input>8 or board_input<0):
-            print("Invalid input try again 0-8")
-        elif(board[board_input]!=0):
-            print("already occupied")
-        else:
-            board[board_input]= (1 if no%2==0 else -1 if no % 2 == 1 else 0)
-            break
+            if (board_input > 8 or board_input < 0):
+                print("Invalid input try again 0-8")
+            elif (board[board_input] != 0):
+                print("already occupied")
+            else:
+                board[board_input] = (1 if no % 2 == 0 else -1 if no % 2 == 1 else 0)
+                break
 def check(board):
     win_con = [
         (0, 1, 2),
@@ -65,8 +68,22 @@ def ai_move(board,no):
             (0, 4, 8),
             (2, 4, 6)
         ]
+        pos2 = [
+            (0,4,8),
+            (2,4,6)
+        ]
         for a, b, c in pos:
-            if sum([board[a], board[b], board[c]]) == 2:
+            if sum([board[a], board[b], board[c]]) == -2:
+                if (board[a] == 0):
+                    return a
+                    break
+                if (board[b] == 0):
+                    return b
+                    break
+                if (board[c] == 0):
+                    return c
+                    break
+            elif sum([board[a], board[b], board[c]]) == 2:
                 if (board[a] == 0):
                     return a
                     break
@@ -77,11 +94,19 @@ def ai_move(board,no):
                     return c
                     break
         else:
-            for i in range(0, 9):
-                u = random.randrange(0, 9)
-                if board[u] == 0:
-                    break
-            return u
+            for a,b,c in pos2:
+                if board[a]==1 and board[b]==-1 and board[c]==1 :
+                    pos = [1, 3, 5, 7]
+                    for i in pos:
+                        if board[i] == 0:
+                            return i
+                            break
+            else:
+                for i in range(0, 9):
+                    u = random.randrange(0, 9)
+                    if board[u] == 0:
+                        break
+                return u
 def play():
     board = create_board()
     for _ in range(9):
@@ -89,8 +114,7 @@ def play():
         print_board(board,False)
         print("status of the board")
         print_board(board,True)
-        print("ai move",ai_move(board,_))
-        plays(board,_)
+        plays(board,_,True)
         if(check(board)==1):
             print_board(board, True)
             print("X has won")
